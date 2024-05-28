79d2d564bf804a175f3fcf23d00213ca87a51f00
sayed <sulimansayed003@gmail.com>
2024-05-28 14:59:45 +0500

    nodejs Integrations

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: "main",
