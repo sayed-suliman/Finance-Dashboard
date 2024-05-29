89d3673334476d82b72d56de731d11fac358a6ff
sayed <sulimansayed003@gmail.com>
2024-05-29 15:06:45 +0500

    Mongoose setup integrations

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 import cors from "cors";
-import dotenv  from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 
 // Configurations
 
 dotenv.config();
-const app =express();
+const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // mongoose setup
 
+const PORT = process.env.PORT;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.error(`${error} did not connect`));
