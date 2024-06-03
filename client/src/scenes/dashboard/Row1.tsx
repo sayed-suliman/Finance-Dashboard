10a39ac56d37a49d27c4e0aa628cc18578173255
sayed <sulimansayed003@gmail.com>
2024-06-03 15:24:40 +0500

    Recharts integration just started

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,22 +1,55 @@
-import DashboardBox from '@/components/DashboardBox'
-import { useGetKpisQuery } from '@/state/api'
-import React from 'react'
+import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from "@/state/api";
+import React from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-type Props = {}
+type Props = {};
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Row1 = (props: Props) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const {data} = useGetKpisQuery();
-  console.log('data : ', data)
+  const { data } = useGetKpisQuery();
   return (
     <>
-      <DashboardBox gridArea="a"></DashboardBox>
+      <DashboardBox gridArea="a">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data = {data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fill="#8884d8"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </DashboardBox>
       <DashboardBox gridArea="b"></DashboardBox>
       <DashboardBox gridArea="c"></DashboardBox>
     </>
-  )
-}
+  );
+};
 
-export default Row1
+export default Row1;
