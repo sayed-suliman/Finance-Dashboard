1565c37b0bead39791d724ce3f4caa3a91486f2f
sayed <sulimansayed003@gmail.com>
2024-06-07 15:09:15 +0500

    jsut a bit of work updto 2:41:41 in the video

---
+++ client/src/components/BoxHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FlexBetween from './flexBetween'
+import { useTheme } from '@emotion/react'
+
+type Props = {}
+
+const BoxHeader = (props: Props) => {
+const {palette} = useTheme();
+
+  return (
+    <FlexBetween color={}>
+
+    </FlexBetween>
+  )
+}
--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -5,7 +5,6 @@ import React, { useMemo } from "react";
 import {
   Area,
   AreaChart,
-  CartesianGrid,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -44,17 +43,25 @@ const Row1 = (props: Props) => {
             height={400}
             data={revenueExpenses}
             margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
+              top: 15,
+              right: 25,
+              left: -10,
+              bottom: 60,
             }}
           >
             {/* this is background */}
             {/* <CartesianGrid strokeDasharray="3 3" /> */}
 
             <defs>
-              <linearGradient id="colorRevenue" xl="0" y1="0" x2="0" y2="1">
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={palette.primary[300]}
+                stopOpacity={0.5}
+                />
+                <stop offset="95%" stopColor={palette.primary[300]}
+                stopOpacity={0}
+                />
+              </linearGradient>
+              <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="5%" stopColor={palette.primary[300]}
                 stopOpacity={0.5}
                 />
